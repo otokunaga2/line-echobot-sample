b73dfdba98153beb833f1a8ab6fc0867b20be9c3
Seiki Tokunaga <otokuna46@gmail.com>
2016-11-21 02:38:50 +0900

    try push message

--- web.rb
+++ web.rb
@@ -53,8 +53,11 @@ post '/callback' do
         end
         begin
           logger.debug("reply message_test1 #{reply_token}, #{message}")
-          client.reply_message(reply_token, message)
+          #client.reply_message(reply_token, message)
+          response = client.push_message("5234458995507", message)
+
           logger.debug("post reply message :))))))))))")
+          logger.debug("response =#{response}")
         rescue => e
           logger.warn(e)
         end
